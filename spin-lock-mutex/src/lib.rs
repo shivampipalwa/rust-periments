3c1e7192cc9d2eab008d4e1fb284c0a8139f8208
shiv3am <shivampipalwa1005@gmail.com>
2026-06-20 19:20:29 +0530

    add: compare_exchange and orderings

--- spin-lock-mutex/src/lib.rs
+++ spin-lock-mutex/src/lib.rs
@@ -21,11 +21,27 @@ impl<T> Mutex<T> {
         }
     }
     pub fn with_lock<R>(&self, f: impl FnOnce(&mut T) -> R) -> R {
-        while self.locked.load(Ordering::Relaxed) != UNLOCKED {}
+        // looping on compare_exchange is very inefficient especially as the number of thread/cores increases
+        // this is because all the cores will try to get the ownership of the memory location
+        // compare_exchange is an inherently expensive proposition as it needs to coordinate exclusive access among core
+        //
+        // hence we use MESI protocol
+        //
+        // since we are using a separeate look, it is okay if compare_exchange fails for some other reason(`)
+        while self
+            .locked
+            .compare_exchange_weak(UNLOCKED, LOCKED, Ordering::Acquire, Ordering::Relaxed)
+            .is_err()
+        {
+            // MESI protocol
+            // this just needs shared access to the memory and not exclusive acess(as in compare_exchange)
+            // hence it is more efficient
+            while self.locked.load(Ordering::Relaxed) == LOCKED {}
+        }
         self.locked.store(LOCKED, Ordering::Relaxed);
         // Safety: We hold the lock, therefore we can create a mutable reference
         let ret = f(unsafe { &mut *self.v.get() });
-        self.locked.store(UNLOCKED, Ordering::Relaxed);
+        self.locked.store(UNLOCKED, Ordering::Release);
         ret
     }
 }
@@ -39,11 +55,11 @@ mod tests {
     #[test]
     fn it_works() {
         let counter = Arc::new(Mutex::new(0));
-        let handles: Vec<_> = (0..10)
+        let handles: Vec<_> = (0..100)
             .map(|_| {
                 let counter = Arc::clone(&counter);
                 thread::spawn(move || {
-                    for _ in 0..100 {
+                    for _ in 0..1000 {
                         counter.with_lock(|v| *v += 1);
                     }
                 })
@@ -52,6 +68,6 @@ mod tests {
         for handle in handles {
             let _ = handle.join();
         }
-        assert_eq!(counter.with_lock(|v| *v), 10 * 100);
+        assert_eq!(counter.with_lock(|v| *v), 100 * 1000);
     }
 }
